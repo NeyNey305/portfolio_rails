20ea376229686cf7aea0f2fe98a954fe23c2457e
Jenee Washington <jeneewashington@icloud.com>
2016-12-12 23:08:52 -0500

    Switched to postrgesql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161210192956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "welcomes", force: :cascade do |t|
     t.string   "index"
     t.datetime "created_at", null: false
